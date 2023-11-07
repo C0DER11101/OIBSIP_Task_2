f8da233c7da263136b33d168c3bb4de8f4368835
Priyanuj Bora <while.alive.program@gmail.com>
2023-11-07 05:48:13 +0530

    Number Guessing Game

--- main.java
+++ main.java
@@ -3,9 +3,6 @@ import java.util.Random;                   // for random number
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.stage.*;
@@ -21,25 +18,25 @@ public class main extends Application {
 
 	Random random=new Random();
 
-	int randInt=random.nextInt(50)+1;
+	int randInt=random.nextInt(50)+1;     // genrating a random number within the range 1 to 50
 
 	@Override
 	public void start(Stage mainStage){
-		mainStage.setTitle("Number Guessing Game");
+		mainStage.setTitle("Number Guessing Game");          // the window shows the name of the game as its title
 
-		Label numLabel=new Label("Enter a number: ");
+		Label numLabel=new Label("Enter a number: ");        // a label that contains the text given in Label()
 
-		TextField numberInput=new TextField();
+		TextField numberInput=new TextField();          // create an input field where the user can enter his/her number
 
-		Button guess=new Button("Check");
+		Button guess=new Button("Check");        // a check button for checking whether the entered number is the actual number
 
-		Label attemptLabel=new Label();   // displaying the number of attempts
-		attemptLabel.setText("Attempts: "+numAttempts);
+		Label attemptLabel=new Label();   // label displaying the number of attempts
+		attemptLabel.setText("Attempts: "+numAttempts);       // displays the number of attempts made by the user
 
-		guess.setOnAction(e -> {
+		guess.setOnAction(e -> {                      // actions to perform when the check button is clicked
 			int errCode=checkNum(numberInput);
 
-			if(numAttempts>10){
+			if(numAttempts>9){
 				gameOver();
 				closeWindow(mainStage);
 			}
@@ -52,7 +49,7 @@ public class main extends Application {
 				else if(errCode<0){
 					numberStatus(errCode);
 					numAttempts++;
-					attemptLabel.setText("Attempts: "+numAttempts);
+					attemptLabel.setText("Attempts: "+numAttempts);   // update the number of attempts
 				}
 
 				else if(errCode>0){
@@ -72,19 +69,21 @@ public class main extends Application {
 
 
 		GridPane layout1=new GridPane();
-		layout1.setPadding(new Insets(10, 10, 10, 10));
-		layout1.setVgap(8);
-		layout1.setHgap(10);
+		layout1.setPadding(new Insets(10, 10, 10, 10));     // setting the padding between the labels, input fields, buttons and the window border
+		layout1.setVgap(8); // setting vertical padding between labels, input fields and buttons
+		layout1.setHgap(10); // setting horizontal padding between labels, input fields and buttons
 
-		layout1.setConstraints(numLabel, 0, 0);
+		// adding the labels, input fields and buttons to the layout
+		layout1.setConstraints(numLabel, 0, 0);       // it's a grid, so we can position the labels, input fields and buttons based on the column value and row value(column value first, then the row value)
 		layout1.setConstraints(numberInput, 1, 0);
 		layout1.setConstraints(guess, 0, 1);
 		layout1.setConstraints(attemptLabel, 3, 0);
-
 		layout1.getChildren().addAll(numLabel, numberInput, guess, attemptLabel);
 
+		// putting the layout into the scene with specified dimensions(width x height)
 		scene1=new Scene(layout1, 500, 250);
 
+		// setting the scene in the main window(the window that will be displayed) so that labels, input fields and buttons are visible to the user
 		mainStage.setScene(scene1);
 
 		mainStage.show();
@@ -92,7 +91,8 @@ public class main extends Application {
 
 	private int checkNum(TextField numInp){
 		try{
-			int num=Integer.parseInt(numInp.getText());
+			// checking if the input is actually a number or a string
+			int num=Integer.parseInt(numInp.getText()); // getText() gets the input given by the user
 
 			if(num<randInt){
 				return -1;
@@ -106,37 +106,38 @@ public class main extends Application {
 				return 0;
 			}
 		}
-		catch(NumberFormatException e){
+		catch(NumberFormatException e){     // if the input is string
 			return -2;
 		}
 	}
 
-	private void errorScene(){
+	private void errorScene(){        // error window which will be displayed if the user enters a string
 		Stage window=new Stage();
-		window.initModality(Modality.APPLICATION_MODAL);
+		window.initModality(Modality.APPLICATION_MODAL);    // this window will have high priority, as long as this window is open, user cannot do anything on the main window until he/she presses the OK button on this window and it cannot be resized
 		window.setTitle("ERROR");
-		window.setMinWidth(250);
+		window.setMinWidth(350);
 
 		Label errLabel=new Label("INVALID INPUT! PLEASE ENTER AN INTEGER!");
 
 		Button closeButton=new Button("OK");
 		closeButton.setOnAction(e -> window.close());
 
-		VBox errLayout=new VBox();
+		VBox errLayout=new VBox(10);
+		errLayout.setPadding(new Insets(10, 10, 10, 10));
 		errLayout.getChildren().addAll(errLabel, closeButton);
-		errLayout.setAlignment(Pos.CENTER);
+		errLayout.setAlignment(Pos.CENTER);  // position the label and the button at the center of the window
 
 		scene2=new Scene(errLayout);
 
 		window.setScene(scene2);
-		window.showAndWait();
+		window.showAndWait();       // show the window and wait until the user gives his/her response
 	}
 
-	private void numberStatus(int code){
+	private void numberStatus(int code){     // this window is also just like the error window above, but it will display the status of user input(whether the number entered by the user is correct or not)
 		Stage window=new Stage();
 		window.initModality(Modality.APPLICATION_MODAL);
 		window.setTitle("Status");
-		window.setMinWidth(250);
+		window.setMinWidth(350);
 
 		Label statusLabel=new Label();
 
@@ -150,7 +151,8 @@ public class main extends Application {
 		Button closeButton=new Button("OK");
 		closeButton.setOnAction(e -> window.close());
 
-		VBox statusLayout=new VBox();
+		VBox statusLayout=new VBox(10);
+		statusLayout.setPadding(new Insets(10, 10, 10, 10));
 		statusLayout.getChildren().addAll(statusLabel, closeButton);
 		statusLayout.setAlignment(Pos.CENTER);
 
@@ -160,7 +162,7 @@ public class main extends Application {
 		window.showAndWait();
 	}
 
-	private void closeWindow(Stage mainWin){
+	private void closeWindow(Stage mainWin){                 // close the window when the user enters the correct number
 		Button closeButton=new Button("Close");
 
 		closeButton.setOnAction(e -> mainWin.close());
@@ -175,11 +177,11 @@ public class main extends Application {
 		mainWin.show();
 	}
 
-	private void gameOver(){
+	private void gameOver(){            // when maximum attempts have been made by the user and no more attempts are possible then display this window
 		Stage window=new Stage();
 		window.initModality(Modality.APPLICATION_MODAL);
 		window.setTitle("Status");
-		window.setMinWidth(250);
+		window.setMinWidth(350);
 
 		Label statusLabel=new Label();
 		statusLabel.setText("You have reached the maximum attempt limit.");
@@ -187,7 +189,8 @@ public class main extends Application {
 		Button closeButton=new Button("OK");
 		closeButton.setOnAction(e -> window.close());
 
-		VBox statusLayout=new VBox();
+		VBox statusLayout=new VBox(10);
+		statusLayout.setPadding(new Insets(10, 10, 10, 10));
 		statusLayout.getChildren().addAll(statusLabel, closeButton);
 		statusLayout.setAlignment(Pos.CENTER);
 
